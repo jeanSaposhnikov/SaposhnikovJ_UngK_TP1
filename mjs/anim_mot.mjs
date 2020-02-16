b6ae7b068c2cf7338b3e6c41399974aa05d67bcb
jeanSaposhnikov <jean.saposhnikov@hotmail.fr>
2020-02-16 11:48:22 -0500

    module pour génération des mots

---
+++ mjs/anim_mot.mjs
@@ -0,0 +1,16 @@
+export class anim_mot {
+    constructor(conteneurParent,mots) {
+      this.conteneurParent = conteneurParent;
+      this.mots=mots;
+      this.creerMot();
+    }
+    
+    creerMot(){
+        let mot=document.createElement('p');
+        mot.innerHTML=this.mots[Math.floor(Math.random()*Math.floor(this.mots.length))];
+        this.conteneurParent.appendChild(mot);
+        mot.classList.add('animMot');
+    }
+    
+    
+}
---
+++ mjs/main.mjs
@@ -0,0 +1,6 @@
+import {anim_mot} from "./anim_mot.mjs";
+let conteneurParent=document.querySelector("#zoneAnim");
+let mots=["html","Css","Sass","Javascript","SCRUM"];
+setInterval(function(){
+    const animMot=new anim_mot(conteneurParent,mots);
+},3000);
