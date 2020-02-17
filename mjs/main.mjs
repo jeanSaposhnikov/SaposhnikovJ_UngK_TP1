6b7213ff165659bab2464ca5e3c19d2ffe04262a
Kenny-Ung <60615000+Kenny-Ung@users.noreply.github.com>
2020-02-16 20:49:46 -0500

    Horaire.mdj

--- mjs/main.mjs
+++ mjs/main.mjs
@@ -1,9 +1,18 @@
 import {anim_mot} from "./anim_mot.mjs";
+import {Horaire} from "./Horaire.mjs";
+import {config_horaire} from "./config_horaire.mjs";
 
+/* Zone animée */
 let conteneurParent=document.querySelector("#zoneAnim");
 let mots=["html","Css","Sass","Javascript","SCRUM"];
-
 // setInterval(function(){
 //     const animMot=new anim_mot(conteneurParent,mots);
 // },3000);
+
+/* Horaire */
+const elmHoraire = document.querySelector('.HoraireAnim');
+let mon_Horaire = new Horaire(elmHoraire, config_horaire);
+
+mon_Horaire.creerHoraire();
+
 
