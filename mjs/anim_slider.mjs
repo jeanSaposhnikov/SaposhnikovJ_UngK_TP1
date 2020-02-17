138be758a502bc11fdfd69cc7003a82a45ed0c89
jeanSaposhnikov <jean.saposhnikov@hotmail.fr>
2020-02-16 22:06:04 -0500

    Dévelopement

--- mjs/anim_slider.mjs
+++ mjs/anim_slider.mjs
@@ -1,11 +1,15 @@
 export class anim_slider {
-    constructor(element) {
+    constructor(element,couleurs) {
         this.element=element;
+        this.couleurs=couleurs;
         this.anim();
+        
     }
 
     anim() {
+        
         this.element.style.opacity=1;
+        this.element.style.color=this.couleurs[Math.floor(Math.random()*Math.floor(this.couleurs.length))];
         console.log(this.element);
     }
 }
